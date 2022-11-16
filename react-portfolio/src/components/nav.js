c42f939c913a5090d8b572fcba0bdb92a74c48bc
Kyle Larsen <kylelarsen.business@gmail.com>
2022-11-15 18:31:37 -0700

    small changes to the nav component.

--- react-portfolio/src/components/nav.js
+++ react-portfolio/src/components/nav.js
@@ -3,6 +3,9 @@ import { useLocation } from 'react-router-dom';
 import { Link } from 'react-router-dom';
 
 function Nav() {
+    const location = useLocation();
+    const { pathname } = location
+    const splitLocation = pathname.split('/');
     return(
         <nav>Portfolio</nav>
     )
