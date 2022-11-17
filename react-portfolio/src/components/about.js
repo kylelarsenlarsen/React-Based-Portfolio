91c01d7a91076e74dc969ad3145f68e400acaac6
Kyle Larsen <kylelarsen.business@gmail.com>
2022-11-16 19:11:19 -0700

    build the about.js nav page.

--- react-portfolio/src/components/about.js
+++ react-portfolio/src/components/about.js
@@ -3,15 +3,18 @@ import React from "react";
 
 const About = () => {
     return (
-        <div>
-            <div>
-                <div>
-                    <h1></h1>
-                        <br></br>
-                        <p>
-                        I'm a boos aas bich
-                        </p>
-                        <br></br>
+        <div className="about-section">
+            <div className="about-container">
+                <div className="about-text">
+                    <h1>Welcome!</h1>
+                    <br></br>
+                    <p>
+                        Hello! I'm Kyle from Utah County, Utah. I'm currently a student enrolled in the full stack development course at University of Utah.
+                    </p>
+                    <br></br>
+                </div>
+                <div className="about-image">
+                    <img></img>
                 </div>
             </div>
         </div>
---
+++ react-portfolio/src/nav-pages/about.js
@@ -0,0 +1,20 @@
+import React from "react";
+import Header from "../components/header";
+import Nav from "../components/nav";
+import About from "../components/about";
+import Footer from "../components/footer";
+
+const AboutPage = () => {
+    return (
+        <>
+        <div>
+            <Header/>
+            <Nav/>
+            <About/>
+            <Footer/>
+        </div>
+        </>
+    );
+};
+
+export default AboutPage;
